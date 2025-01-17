220a4820c1303e1631783651d6b90d7763b64a31
ZapDragon <17253171+ZapDragon@users.noreply.github.com>
2025-01-16 22:30:51 -0800

    Update gg-deployment.sh
    Lots of fixes. Trying to pass the Endpoint as an external variable.

--- gg-deployment.sh
+++ gg-deployment.sh
@@ -1,5 +1,3 @@
-#!/bin/env bash
-
 ## Group Guardian Node Bootstrap
 
 #
@@ -11,41 +9,41 @@
 # First function called, after script init.
 function Init
 {
-	apt update
-	apt full-upgrade -y
-	apt install curl beep -y
-	clear
-	if [ $(which pwsh) ]; then
-		echo "PowerShell is already installed"
-	else
-		InstallPowerShell
-	fi
-	
-	GetPayload
+    apt update
+    apt full-upgrade -y
+    apt install curl beep -y
+    clear
+    if [ $(which pwsh) ]; then
+        echo "PowerShell is already installed"
+    else
+        InstallPowerShell
+    fi
+
+    GetPayload
 }
 
 # Get and install Powershell - Yes, PowerShell. Judge me all you want.
 function InstallPowerShell
 {
-	echo "Installing PowerShell"
-	curl -s https://github.com/PowerShell/PowerShell/releases/download/v7.4.6/powershell_7.4.6-1.deb_amd64.deb -o pwsh_7.4.6.deb
-	dpkg -i pwsh_7.4.6.deb
-	apt install -f
-	rm pwsh_7.4.6.deb
-	echo "Installed PowerShell"
+    echo "Installing PowerShell"
+    curl -s https://github.com/PowerShell/PowerShell/releases/download/v7.4.6/powershell_7.4.6-1.deb_amd64.deb -o pwsh_7.4.6.deb
+    dpkg -i pwsh_7.4.6.deb
+    apt install -f
+    rm pwsh_7.4.6.deb
+    echo "Installed PowerShell"
 }
 
 # Get the Endpoint URL, and execute the downloaded payload.
-# Any errors here, and this script will exist without any other prompts.
+# Any errors here, and this script will exit without any other prompts.
 function GetPayload
 {
-	GGEP='none'
-	while [[ "$GGEP" == "none" || "$GGEP" == "" ]]; do
-		beep
-		read -p "Please provide an endpoint: " GGEP
-	done
-	curl -s $GGEP -o Payload.ps1
-	pwsh -f ./Payload.ps1
+    if [[ "$GGEP" != $null && "$GGEP" != "" ]]; then
+        echo "EndPoint is $GGEP"
+        curl -s $GGEP -o Payload.ps1
+        pwsh -f ./Payload.ps1
+    else
+        echo "You must specificy an endpoint as an argument to this script"
+    fi
 }
 
 Init
